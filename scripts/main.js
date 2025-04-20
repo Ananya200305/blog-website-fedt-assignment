76d78007b7c4a872045a7c285e3fa875b2e4778c
Ananya Aggarwal <ananyaaggarwal@Ananyas-MacBook-Air.local>
2025-04-20 15:32:09 +0530

    Changes made by Ananya

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,12 @@ const toggleTheme = () => {
     const currentTheme = document.documentElement.getAttribute("data-theme");
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
+
+    // change logo theme
+
+    const logo = document.querySelector(".logo");
+    if(logo){
+        const newSrc = newTheme === "dark" ? logo.dataset.dark : logo.dataset.light;
+        logo.src = newSrc;
+    }
 }
